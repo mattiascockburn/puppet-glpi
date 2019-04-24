afed497c3dc828979a6122feab7d17acde3baa29
Mattias Giese <mattiasgiese@posteo.net>
2019-04-23 20:39:46 -0500

    Simple repo config for RHEL This adds a hacky repo class for enabling scl/epel if desired. Add hiera-data for all classes.

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include glpi
+class glpi(
+  Boolean $manage_database,
+  Boolean $manage_packages,
+  Boolean $manage_repos,
+) {
+  if $manage_repos {
+    contain glpi::repos
+  }
+
+}
