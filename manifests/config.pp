7492fd9ebe82ee3237e209cb6440e3673ec11279
Mattias Giese <mattiasgiese@posteo.net>
2019-04-24 17:40:23 -0500

    config: fix default GRANT on database

--- manifests/config.pp
+++ manifests/config.pp
@@ -19,7 +19,7 @@ class glpi::config (
     user     => $glpi::db_user,
     password => $glpi::db_password,
     host     => $glpi::db_host,
-    grant    => ['*'],
+    grant    => ['ALL'],
     }
   }
 }
