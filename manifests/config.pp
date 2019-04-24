cb3a43a4106d76fbd2f8544924a2b9c8ad621fe9
Mattias Giese <mattiasgiese@posteo.net>
2019-04-24 17:24:33 -0500

    config.pp: try to fix selinux type of files/config dirs

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,6 +6,14 @@
 #   include glpi::config
 class glpi::config (
 ){
+  if $facts['selinux'] {
+    file{"${glpi::archive_dest}/config":
+      seltype => 'httpd_sys_rw_content_t',
+    }
+    file{"${glpi::archive_dest}/files":
+      seltype => 'httpd_sys_rw_content_t',
+    }
+  }
   if $glpi::manage_database {
     mysql::db { $glpi::db_name:
     user     => $glpi::db_user,
