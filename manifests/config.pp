9a5781e67ae0f1c194bde1b156ce5f2f4e9e582e
Mattias Giese <mattiasgiese@posteo.net>
2019-05-13 18:06:46 -0500

    config.pp: fix selinux type of files/ subdirs

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,6 +13,11 @@ class glpi::config (
     file{"${glpi::archive_dest}/files":
       seltype => 'httpd_sys_rw_content_t',
     }
+    ['cache', 'cron', 'dumps', 'graphs', 'lock', 'log', 'pictures', 'plugins', 'rss', 'sessions', 'tmp', 'uploads'].each |String $subdir| {
+      file{"${glpi::archive_dest}/files/_${subdir}":
+        seltype => 'httpd_sys_rw_content_t',
+      }
+    }
     if $glpi::use_ldap {
       # Needed for LDAP Sync
       selboolean{'httpd_can_connect_ldap':
@@ -23,10 +28,11 @@ class glpi::config (
   }
   if $glpi::manage_database {
     mysql::db { $glpi::db_name:
-      user     => $glpi::db_user,
-      password => $glpi::db_password,
-      host     => $glpi::db_host,
-      grant    => ['ALL'],
+      user            => $glpi::db_user,
+      password        => $glpi::db_password,
+      host            => $glpi::db_host,
+      grant           => ['ALL'],
+      mysql_exec_path =>
     }
   }
 }
