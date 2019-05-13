5257d9406fe1806042830a0dec354ee9aeacd5b0
Mattias Giese <mattiasgiese@posteo.net>
2019-05-13 18:37:32 -0500

    Add fusioninventory plugin class
    This is just a skeleton and ost certainly does not work

---
+++ manifests/plugin/fusioninventory.pp
@@ -0,0 +1,30 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include glpi::plugin::fusioninventory
+class glpi::plugin::fusioninventory (
+  $version       = '9.4+1.1',
+  $url           = "https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi${version}/fusioninventory-${version}.tar.bz2",
+  $archive       = "fusioninventory-${version}.tar.bz2",
+  $checksum      = '12bb42d349393c7d3ede72a9f98672d0ecbcc58a87ebfad8f15a2ecefa455e5e',
+  $checksum_type = 'sha256',
+  $archive_dest  = "${glpi::archive_dest}/files/_plugins/fusioninventory",
+){
+  archive { $archive:
+    path            => "/var/tmp/${archive}",
+    source          => $url,
+    checksum        => $checksum,
+    checksum_type   => $checksum_type,
+    extract_command => 'tar xfj %s --strip-components=1',
+    extract         => true,
+    extract_path    => $archive_dest,
+    creates         => "${archive_dest}/README.md",
+    cleanup         => true,
+    user            => $glpi::owner,
+    group           => $glpi::group,
+    require         => File[$glpi::archive_dest],
+    }
+
+}
