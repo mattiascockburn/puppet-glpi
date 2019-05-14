298164059eaa7244c696b2a89bfa25e671b07228
Mattias Giese <mattiasgiese@posteo.net>
2019-05-13 20:06:12 -0500

    fusioninventory: manage prereqs by default
    bzip2 is needed for the extraction of the archive

--- manifests/plugin/fusioninventory.pp
+++ manifests/plugin/fusioninventory.pp
@@ -5,13 +5,17 @@
 # @example
 #   include glpi::plugin::fusioninventory
 class glpi::plugin::fusioninventory (
-  $version       = '9.4+1.1',
-  $url           = "https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi${version}/fusioninventory-${version}.tar.bz2",
-  $archive       = "fusioninventory-${version}.tar.bz2",
-  $checksum      = '12bb42d349393c7d3ede72a9f98672d0ecbcc58a87ebfad8f15a2ecefa455e5e',
-  $checksum_type = 'sha256',
-  $archive_dest  = "${glpi::archive_dest}/plugins/",
+  $manage_prereqs = true,
+  $version        = '9.4+1.1',
+  $url            = "https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi${version}/fusioninventory-${version}.tar.bz2",
+  $archive        = "fusioninventory-${version}.tar.bz2",
+  $checksum       = '12bb42d349393c7d3ede72a9f98672d0ecbcc58a87ebfad8f15a2ecefa455e5e',
+  $checksum_type  = 'sha256',
+  $archive_dest   = "${glpi::archive_dest}/plugins/",
 ){
+  if $manage_prereqs {
+    ensure_packages('bzip2')
+  }
   archive { $archive:
     path            => "/var/tmp/${archive}",
     source          => $url,
